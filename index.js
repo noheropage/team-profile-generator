1120cb08a39431711de1cd1130b437f1779f160f
noheropage <mursepage@gmail.com>
2021-03-02 15:17:50 -0800

    More outlining for various classes

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const inquirer = require('inquirer')
+const fs = require('fs');
+const Employee = require('./lib/Employee')
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
 
+const questions = () => {
+    
+    // inquirer.prompt([
+    //     {
+    //         type: 'list',
+    //         name: 'title',
+    //         choices: ['Add Engineer', 'Add Intern', 'None']
+    //     }
+    // ])
+    
+}
+
+function init() {
+    try {
+        const employee = new Employee;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+init();
