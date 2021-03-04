89bda2a1fd9828121f2dfa3cc1a9129e402f2a50
noheropage <mursepage@gmail.com>
2021-03-04 00:01:30 -0800

    Created variables to store user input into differing employee classes

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 let title = 'manager'
 const team = []
+let newEmployee = ''
 
-const everyoneQuestions = () => {
-    inquirer.prompt([
+const questions = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -25,102 +26,79 @@ const everyoneQuestions = () => {
             message: 'Email address: ',
         },
     ])
-}
+        .then((answers) => {
+            switch (title) {
+                case 'manager':
+                    newEmployee = new Manager(answers.name, answers.ID, answers.email)
+                    return inquirer.prompt(
+                        {
+                            type: 'input',
+                            name: 'office',
+                            message: 'Office #: '
+                        }
+                    )
 
-const managerQ = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'office',
-            message: 'Office #: '
-        }
-    ])
-}
+                case 'engineer':
+                    newEmployee = new Engineer(answers.name, answers.ID, answers.email)
+                    return inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'github',
+                            message: "GitHub username: "
+                        }
+                    ])
 
-const engineerQ = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'github',
-            message: "GitHub username: "
-        }
-    ])
-}
+                case 'intern':
+                    newEmployee = new Intern(answers.name, answers.ID, answers.email)
+                    return inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'school',
+                            message: 'School name: '
+                        }
+                    ])
 
-const internQ = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'school',
-            message: 'School name: '
-        }
-    ])
-}
-
-const addEmployee = () => {
-    inquirer.prompt([
-        // {
-        //     type: 'confirm',
-        //     name: 'add employee',
-        //     message: 'Would you like to add more team members?',
-        // },
-        {
-            type: 'list',
-            name: 'title',
-            choices: ['Add Engineer', 'Add Intern', 'No more employees to add']
-        }
-    ]).then(ans => {
-        switch (ans.title) {
-            case 'Add Engineer':
-                title = 'engineer'
-                return questions();
-            case 'Add Intern':
-                title = 'intern'
-                return questions();
-            default:
-                // end questions
-                break;
-        }
-    })
+                default:
+                    break;
+            }
+        })
+        .then((ans) => {
+            newEmployee.push(ans)
+            return inquirer.prompt([
+                {
+                    type: 'list',
+                    message: 'Would you like to add more team members?',
+                    name: 'title',
+                    choices: ['Add Engineer', 'Add Intern', 'No more employees to add']
+                }
+            ]).then(ans => {
+                team.push(newEmployee)
+                switch (ans.title) {
+                    case 'Add Engineer':
+                        title = 'engineer'
+                        return questions();
+                    case 'Add Intern':
+                        title = 'intern'
+                        return questions();
+                    default:
+                        // end questions
+                        break;
+                }
+            })
+        })
 }
 
 function init() {
     try {
-        everyoneQuestions()
-        .then(() => {
-            switch (title) {
-                case 'manager':
-                    return managerQ();
-                case 'engineer':
-                    return engineerQ();
-                case 'intern':
-                    return internQ();
-                default:
-                    break;
-            }
-        }).then(() => {
-            addEmployee();
-        })
-        // questionLoop(title);
-        // .then() => {
-        //     questionLoop(title);
-        // }
-        // console.log("Manager recorded")
-        // addEmployee().then((inputs) => {
-        //     switch (inputs.title) {
-        //         case 'Add Engineer':
-        //             title = 'engineer'
-        //             return engineerQ()
-        //         case 'Add Intern':
-        //             title = 'intern'
-        //             return internQ()
-        //         default:
-        //             break;
-        //     }
-        // })
+        questions().then(() => {
+            console.log(team);
+            // console.log(newEmployee);
+        });
+        // console.log(team);
     } catch (error) {
         console.log(error);
     }
 }
 
 init();
+// console.log(team);
